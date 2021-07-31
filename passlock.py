92007620c90cfa9ebd554aaddafe4f04193066b1
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-07-31 16:21:01 +0300

    function delete credential

--- passlock.py
+++ passlock.py
@@ -61,3 +61,9 @@ class Credentials():
         method to store a new credential to the credentials list
         """
         Credentials.credentials_list.append(self)
+    
+    def delete_credentials(self):
+        """
+        delete credential method that deletes an account credentials from the credentials_list
+        """
+        Credentials.credentials_list.remove(self)
