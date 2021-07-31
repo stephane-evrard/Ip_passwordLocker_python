08702758a1cff0fc205c98bdc685d977bcedf265
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-07-31 16:34:12 +0300

    create, save, display and login user

--- interface.py
+++ interface.py
@@ -8,3 +8,29 @@ def function():
 	print("              | |    / (_| |\__ \ \__ \    ___  / | |___ (  (_)  ) | |  ___/       ")
 	print("              |_|    \_____| ___/  ___/   |____/  |_____) \_____/|_|  \____        ")
 function()
+
+def create_new_user(username, password):
+    """
+    Function to create a new user with a username and password
+    """
+    new_user = User(username, password)
+    return new_user
+
+def save_user(user):
+    """
+    Function to save a new user
+    """
+    user.save_user()
+
+def display_user():
+    """
+    Function to display existing user
+    """
+    return User.display_user()
+
+def login_user(username, password):
+    """
+    function that checks whether a user exist and then login the user in
+    """
+    check_user = Credentials.verify_user(username, password)
+    return check_user
