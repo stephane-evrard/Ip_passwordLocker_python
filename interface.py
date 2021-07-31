d9df9f39dc7689fa4d004dec2270d1572c0790e5
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-07-31 16:37:12 +0300

    create, save, display, delete, find, and check credential

--- interface.py
+++ interface.py
@@ -34,3 +34,40 @@ def login_user(username, password):
     """
     check_user = Credentials.verify_user(username, password)
     return check_user
+
+def create_new_credential(account, userName, password):
+    """
+    Function that creates new credentials for a given user account
+    """
+    new_credential = Credentials(account, userName, password)
+    return new_credential
+
+def save_credentials(credentials):
+    """
+    Function to save Credentials to the credentials list
+    """
+    credentials.save_details()
+
+def display_accounts_details():
+    """
+    Function that returns all the saved credential
+    """
+    return Credentials.display_credentials()
+
+def delete_credential(credentials):
+    """
+    Function to delete a Credentials from credentials list
+    """
+    credentials.delete_credentials()
+
+def find_credential(account):
+    """
+    Function that finds a Credentials by an account name and returns the Credentials that belong to that account
+    """
+    return Credentials.find_credential(account)
+
+def check_credentials(account):
+    """
+    Function that check if a Credentials exists with that account name and return true or false
+    """
+    return Credentials.if_credential_exist(account)
